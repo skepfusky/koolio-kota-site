acd81a51da140e002523049e29a2acb7d63e3f63
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2021-12-01 01:55:53 +0800

    Formatted scripts.js

--- public/js/comms-scripts.js
+++ public/js/comms-scripts.js
@@ -1,33 +1,33 @@
 window.onload = () => {
-	const tosBtn = document.getElementById('tos-btn');
-	const closeBtn = document.getElementById('close-btn');
-	const expandDong = document.getElementById('card-expand');
-	
-	const tosModal = document.getElementById('tos-modal');
-	const commsBlur = document.getElementById('blur-comms');
-	const aboutBlur = document.getElementById('blur-about');
-	
-	tosBtn.onclick = () => {
-		commsBlur.classList.remove('card-comms');
-		commsBlur.classList.add('card-comms_modal');
-		aboutBlur.classList.remove('card-about-wrap');
-		aboutBlur.classList.add('card-about-wrap_modal');
-		expandDong.style.height='50rem';
-		setTimeout(()=> {
-			tosModal.classList.remove('overlay-hidden');
-			tosModal.classList.add('overlay');
-		}, 170);
-	}
-	
-	closeBtn.onclick = () => {
-			tosModal.classList.remove('overlay');
-			tosModal.classList.add('overlay-hidden');
-		setTimeout(()=>{
-			commsBlur.classList.remove('card-comms_modal');
-			commsBlur.classList.add('card-comms');
-			aboutBlur.classList.remove('card-about-wrap_modal');
-			aboutBlur.classList.add('card-about-wrap');
-			expandDong.style.height='40rem';
-		}, 50)
-	}
-}
+  const tosBtn = document.getElementById("tos-btn");
+  const closeBtn = document.getElementById("close-btn");
+  const expandDong = document.getElementById("card-expand");
+
+  const tosModal = document.getElementById("tos-modal");
+  const commsBlur = document.getElementById("blur-comms");
+  const aboutBlur = document.getElementById("blur-about");
+
+  tosBtn.onclick = () => {
+    commsBlur.classList.remove("card-comms");
+    commsBlur.classList.add("card-comms_modal");
+    aboutBlur.classList.remove("card-about-wrap");
+    aboutBlur.classList.add("card-about-wrap_modal");
+    expandDong.style.height = "50rem";
+    setTimeout(() => {
+      tosModal.classList.remove("overlay-hidden");
+      tosModal.classList.add("overlay");
+    }, 170);
+  };
+
+  closeBtn.onclick = () => {
+    tosModal.classList.remove("overlay");
+    tosModal.classList.add("overlay-hidden");
+    setTimeout(() => {
+      commsBlur.classList.remove("card-comms_modal");
+      commsBlur.classList.add("card-comms");
+      aboutBlur.classList.remove("card-about-wrap_modal");
+      aboutBlur.classList.add("card-about-wrap");
+      expandDong.style.height = "40rem";
+    }, 50);
+  };
+};
